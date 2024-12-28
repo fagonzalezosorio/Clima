6fd7768972c0fdb38542175ebd04fa28db5536e6
Franco Gonzalez <franco.gonzalezo@sansano.usm.cl>
2024-12-28 19:55:25 -0300

    test2

--- Contexto y Equipo.py
+++ Contexto y Equipo.py
@@ -10,19 +10,19 @@ import numpy as np
 # logo = Image.open('images/logo.png')
 # st.sidebar.image(logo, width=180)
 
-# Agrega un título de contexto y una reseña pequeña
-st.markdown('<p style="font-family:Calibri Light; color:Black; font-size: 40px; font-weight:bold; text-shadow: 2px 2px 4px #000000;">Contexto</p>', unsafe_allow_html=True)
-st.markdown('<p style="text-align: justify;">En la actualidad, llevar el control de las actividades desarrolladas, asi como los fenomenos climatologicos es fundamental.</p>', unsafe_allow_html=True)
-st.markdown('<p style="text-align: justify;">El objetivo principal es obtener una forma facil de monitorear esto.</p>', unsafe_allow_html=True)
+# # Agrega un título de contexto y una reseña pequeña
+# st.markdown('<p style="font-family:Calibri Light; color:Black; font-size: 40px; font-weight:bold; text-shadow: 2px 2px 4px #000000;">Contexto</p>', unsafe_allow_html=True)
+# st.markdown('<p style="text-align: justify;">En la actualidad, llevar el control de las actividades desarrolladas, asi como los fenomenos climatologicos es fundamental.</p>', unsafe_allow_html=True)
+# st.markdown('<p style="text-align: justify;">El objetivo principal es obtener una forma facil de monitorear esto.</p>', unsafe_allow_html=True)
 st.markdown("         ")
 st.markdown("         ")
 
-# Agrega el título "Quienes Somos"
-st.markdown('<p style="font-family:Calibri Light; color:Black; font-size: 40px; font-weight:bold; text-shadow: 2px 2px 4px #000000;">Quienes Somos</p>', unsafe_allow_html=True)
-st.markdown("         ")
-st.markdown("         ")
+# # Agrega el título "Quienes Somos"
+# st.markdown('<p style="font-family:Calibri Light; color:Black; font-size: 40px; font-weight:bold; text-shadow: 2px 2px 4px #000000;">Quienes Somos</p>', unsafe_allow_html=True)
+# st.markdown("         ")
+# st.markdown("         ")
 
-col1, col2, col3, col4 = st.columns(4) # 
+# col1, col2, col3, col4 = st.columns(4) # 
 
 
 
