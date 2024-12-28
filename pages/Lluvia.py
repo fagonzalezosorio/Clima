8a89983d0414e6957fb3a33d0b922b2047d525bb
Franco Gonzalez <franco.gonzalezo@sansano.usm.cl>
2024-12-28 19:11:37 -0300

    requirements

--- pages/Lluvia.py
+++ pages/Lluvia.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
+# import plotly.express as px
+# import plotly.graph_objects as go
 import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
+# from PIL import Image
+# import matplotlib.pyplot as plt
 
 ############## Imagenes ########################
 
