3cd89f1be76201ef710de9261dcebc3b9c93a032
Franco Gonzalez <franco.gonzalezo@sansano.usm.cl>
2024-12-28 20:24:27 -0300

    test3

--- Contexto y Equipo.py
+++ Contexto y Equipo.py
@@ -12,10 +12,15 @@ import numpy as np
 
 # # Agrega un título de contexto y una reseña pequeña
 # st.markdown('<p style="font-family:Calibri Light; color:Black; font-size: 40px; font-weight:bold; text-shadow: 2px 2px 4px #000000;">Contexto</p>', unsafe_allow_html=True)
-# st.markdown('<p style="text-align: justify;">En la actualidad, llevar el control de las actividades desarrolladas, asi como los fenomenos climatologicos es fundamental.</p>', unsafe_allow_html=True)
+st.markdown('<p style="text-align: justify;">En la actualidad, llevar el control de las actividades desarrolladas, asi como los fenomenos climatologicos es fundamental.</p>', unsafe_allow_html=True)
 # st.markdown('<p style="text-align: justify;">El objetivo principal es obtener una forma facil de monitorear esto.</p>', unsafe_allow_html=True)
 st.markdown("         ")
 st.markdown("         ")
+
+col1,col2 = st.sidebar.columns(2)
+# col1.image(lluvia, width=90)
+# col2.image(paragua, width=94)
+st.sidebar.title("Filtros2")
 
 # # Agrega el título "Quienes Somos"
 # st.markdown('<p style="font-family:Calibri Light; color:Black; font-size: 40px; font-weight:bold; text-shadow: 2px 2px 4px #000000;">Quienes Somos</p>', unsafe_allow_html=True)
